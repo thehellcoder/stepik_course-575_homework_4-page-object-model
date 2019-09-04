3bd1bcd7b9b0e083cd424a0c48f9ab95d04a8ee6
thehellcoder <the.hell.coder@gmail.com>
2019-09-04 08:47:59 +0300

    Really initial commit

---
+++ conftest.py
@@ -0,0 +1,33 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en-us', \
+        help='Choose default language')
+    parser.addoption('--browser_name', action='store', default='chrome', \
+        help='Chroose browser (chrome or firefox)')
+
+@pytest.fixture(scope='function')
+def browser(request):
+    language = request.config.getoption('language')
+    if language == '':
+        raise pytest.UsageError('--language could not be empty str')
+    browser_name = request.config.getoption('browser_name')
+    browser = None
+    if browser_name == 'chrome':
+        options = Options()
+        options.add_experimental_option('prefs',
+                                        {
+                                            'intl.accept_languages': language
+                                        })
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == 'firefox':
+        profile = webdriver.FirefoxProfile()
+        profile.set_preference('intl.accept_languages', language)
+        browser = webdriver.Firefox(firefox_profile=profile)
+    else:
+        raise pytest.UsageError('--browser_name should be chrome or firefox')
+    browser.implicitly_wait(30)
+    yield browser
+    browser.quit()
---
+++ test_main_page.py
@@ -0,0 +1,6 @@
+# Run me with: pytest -v --tb=line --language=en test_main_page.py
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    login_link = browser.find_element_by_css_selector("#login_link")
+    login_link.click()
