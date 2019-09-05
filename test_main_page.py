d271e4144d757d0085ce795dc03518f57565099e
thehellcoder <the.hell.coder@gmail.com>
2019-09-05 09:51:46 +0300

    add LoginPageLocators class and LoginPage class with assertions

--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,17 @@
 # Run me with: pytest -v --tb=line --language=en test_main_page.py
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 MAIN_PAGE_URL = 'http://selenium1py.pythonanywhere.com/'
 
 def test_guest_should_see_login_link(browser):
-    page = MainPage(browser, MAIN_PAGE_URL)
-    page.open()
-    page.should_be_login_link()
+    main_page = MainPage(browser, MAIN_PAGE_URL)
+    main_page.open()
+    main_page.should_be_login_link()
 
 def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, MAIN_PAGE_URL)
-    page.open()
-    page.go_to_login_page()
+    main_page = MainPage(browser, MAIN_PAGE_URL)
+    main_page.open()
+    main_page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
